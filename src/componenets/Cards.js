0fdf1ba7e82d821b19d004657425c6c8225c16b9
Antor <md.antor1155@gmail.com>
2023-05-04 08:54:53 +0600

    added title and other basic parts to news secton:

--- src/componenets/Cards.js
+++ src/componenets/Cards.js
@@ -7,10 +7,14 @@ const Cards = ({data}) => {
     return (
         <div className='cards'>
             <img src={urlToImage? urlToImage : notFound} alt="Img of the rews"></img>
-            <p>{author ? author: "N/A"}</p>
-            <p>{title ? title : "N/A"}</p>
-            <p>{source.name ? source.name : "N/A"}</p>
-            <p>{publishedAt? publishedAt : "N/A"}</p>
+            <ul>
+                <li><b>Author: </b>{author ? author: "N/A"}</li>
+                <li><b>Source: </b>{source.name ? source.name : "N/A"}</li>
+                <li><b>Date: </b>{publishedAt? publishedAt : "N/A"}</li>
+            </ul>
+
+            <p><b>Title: </b> {title ? title : "N/A"}</p>
+            <p></p>
         </div>
     );
 };
