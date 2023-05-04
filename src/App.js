a09f35cee507c3a853e8609fb303e7f6c616cc5d
Antor <md.antor1155@gmail.com>
2023-05-05 00:56:38 +0600

    context api for theme fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import Head from './componenets/Head';
 import Navigation from './componenets/Navigation';
 import NewsSection from './componenets/NewsSection';
 import Footer from './componenets/Foot';
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import FourZero from './componenets/fourzero';
+import {ThemeContext} from './componenets/ThemeProvider';
 
 function App() {
+
   return (
-    <div className="App">
-      <Head></Head>
-      <Navigation></Navigation>
-      <Routes>
-        <Route path="/" element={<NewsSection></NewsSection>}></Route>
-        <Route path="/:id" element={<NewsSection></NewsSection>}></Route>
-        <Route path="*" element={<FourZero></FourZero>} />
-      </Routes>
-      <Footer></Footer>
-    </div>
+    <ThemeContext.Provider value={"hello2"}>
+      <div className="App">
+        <Head></Head>
+        <Navigation></Navigation>
+        <Routes>
+          <Route path="/" element={<NewsSection></NewsSection>}></Route>
+          <Route path="/:id" element={<NewsSection></NewsSection>}></Route>
+          <Route path="*" element={<FourZero></FourZero>} />
+        </Routes>
+        <Footer></Footer>
+      </div>
+    </ThemeContext.Provider >
   );
 }
 
--- src/componenets/Head.js
+++ src/componenets/Head.js
@@ -1,11 +1,13 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import { Link } from 'react-router-dom';
 import "./head.css";
 import { useNavigate } from 'react-router-dom';
 
-
+import { useContext } from 'react';
+import {ThemeContext} from "./ThemeProvider";
 
 function Head(){
+        let theme  = useContext(ThemeContext);
 
         let [topic, setTopic] = useState("")
         let navigate = useNavigate()
@@ -19,13 +21,16 @@ function Head(){
                 setTopic("")
             }
         }
+        function handleTheme(){
+            console.log("dark is: ", theme)
+        }
         return (
             <header className='headSection'>
                 <div className='search'>
                     <input type='text' onChange={handleChange} value={topic} maxLength={50} placeholder='topic'></input>
                     <button onClick={handleSearch}> Search</button>
                 </div>
-                <h1>News Feed</h1>
+                <h1><button className='themeChanger' onClick={handleTheme}><img src={require("../asset/icons/sun.png")} alt="sun"></img></button> News Feed <button className='themeChanger' onClick={handleTheme}><img src={require("../asset/icons/moon.png")} alt="moon icon"></img></button></h1>
                 <aside>
                     <Link to="https://www.linkedin.com/in/md-antor/" target='_blank'>
                         <img src={require("../asset/giphy.gif")} alt="add" />
